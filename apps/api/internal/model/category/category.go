a887a20466f0448a17a531b84fe2a3d207a82554
Shani Sharma <shanisharrma@Shanis-MacBook-Air.local>
2025-12-21 20:34:01 +0530

    feat: Added models and database migrations

---
+++ apps/api/internal/model/category/category.go
@@ -0,0 +1,11 @@
+package category
+
+import "github.com/shanisharrma/tasker/internal/model"
+
+type Category struct {
+	model.Base
+	UserID      string  `json:"userId" db:"user_id"`
+	Name        string  `json:"name" db:"name"`
+	Color       string  `json:"color" db:"color"`
+	Description *string `json:"description"`
+}
